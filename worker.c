a3877ccef3683253464fc01e30a2522a331b1c65
Marco Preciado <mac46005@gmail.com>
2023-02-15 20:54:27 -0600

    need to understand shared memory

---
+++ worker.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    int secondsStay, nanoStay;
+
+    secondsStay = atoi(argv[1]);
+    nanoStay = atoi(argv[2]);
+
+    
+    return 0;
+}
