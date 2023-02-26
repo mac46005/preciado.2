5607d72796ca0ea4e899c202e8248c539a63240c
Marco Preciado <mac46005@gmail.com>
2023-02-26 10:36:53 -0600

    having trouble with shared memory

--- oss.c
+++ oss.c
@@ -13,9 +13,21 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 #include "igetopt_helper.h"
 #include "ioss.h"
+#include <math.h>
 #include "shrdmem.h"
+
+
+
+
+
+
+
+
+
+
 
 struct PCB{
     int occupied;
@@ -23,25 +35,65 @@ struct PCB{
     int start_seconds;
     int start_nano;
 };
-// int shmid1 = shmget(SHMKEY_1, BUff_SZ, 0777 | IPC_CREAT);
-//         printf("key :%d\n" ,shmid1);
-//         if(shmid1 == -1){
-//             fprintf(stderr, "Error in shmget\n");
-//         }
-//         int* paddr = (int*)(shmat(shmid1, 0,0));
-//         shmdt(paddr);
-//         shmctl(shmid1, IPC_RMID, NULL);
+
+struct PCB create_pcb(int occupied,pid_t pid, int start_seconds, int start_nano){
+    struct PCB new_child;
+    new_child.occupied = occupied;
+    new_child.pid = pid;
+    new_child.start_seconds = seconds;
+    new_child.start_nano = nanoseconds;
+    return new_child;
+}
+
 struct PCB process_table[20];
 
+
+
+
+
+
+
+
+
+
+int getrandsec(){
+    srand(time(0));
+    int num = (rand() % time_limit + 1);
+    return num;
+}
+int getrandnano(){
+    srand(time(0));
+    int num = (rand() % 999999999);
+    return num;
+}
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char* argv[]){
+    
+    
     pid_t pid;
     int count = 0;
     int status;
     pid_t terminated_child;
     pid_t simul_pid[simul];
     int a_child_terminated = 0;
+
+
+
+
     if(process_getopt(argc, argv) == 0){
+
         while(proc != 0){
+
+
             increment_clock();
             int shdm_id = shmget(SHMKEY_1, BUff_SZ, 0777 | IPC_CREAT);
             int* paddr = (int*)(shmat(shdm_id, 0, 0));
@@ -51,56 +103,84 @@ int main(int argc, char* argv[]){
 
 
 
+
+
+
+
+            //SIMULTANOUS PROCESS
             if(simul != 0){
                 pid = fork();
                 if(pid == 0){
                     child();
-                }
-                    struct PCB new_child;
-                    new_child.occupied = 1;
-                    printf("child pid %d\n", pid);
-                    sleep(1);
-                    new_child.pid = pid;
-                    new_child.start_seconds = seconds;
-                    new_child.start_nano = nanoseconds;
+                }else{
+
+                    struct PCB new_child = create_pcb(1, pid, seconds, nanoseconds);
+
                     process_table[count] = new_child;
+
                     simul_pid[count] = pid;
+
+
                     count++;
                     proc--;
-                
+                    simul--;
+                }
             }
 
+
+
+
+            //Check if child terminated
             int i;
             for(i = 0; i <= count; i++){
                 if((terminated_child = waitpid(simul_pid[count],&status, WNOHANG)) == 0){
                     a_child_terminated = 0;
-                    terminated_child = simul_pid[count];
-                    print_process_table();
                 }
             }
+
+
+
+
+            //Actions if child terminated
             if(a_child_terminated == 0){
-                printf("a child terminated: %d\n", terminated_child);
+
                 for(i = 0;i < count; i++){
+
                     if(process_table[i].pid = terminated_child){
+
                         process_table[i].occupied = 0;
-                        print_process_table();
+
                     }
+
                 }
-                if(simul == 0){
+
+                //Create non-simultanous children
+                if(simul == 0 && proc != 0){
+
                     pid = fork();
+
                     if(pid == 0){
+
                         child();
+
                     }else{
-                        struct PCB new_child;
-                        new_child.occupied = 1;
-                        new_child.pid = pid;
-                        new_child.start_seconds = seconds;
-                        new_child.start_nano = nanoseconds;
+
+                        struct PCB new_child = create_pcb(1, pid, seconds, nanoseconds);
+
+                        process_table[count] = new_child;
+
                         count++;
+                        proc--;
+
                         wait(&status);
                     }
                 }
             }
+
+
+
+            shmdt(paddr);
+            shmctl(shdm_id, IPC_RMID, NULL);
         }
     }else{
         help_message();
@@ -161,12 +241,18 @@ int process_getopt(int argc, char* argv[]){
     }
     return exit;
 }
+
+
 void option_err(char opt, char* value){
     fprintf(stderr, "ERROR: option %c has invalid value %s\n", opt, value);
 }
+
+
 void exit_message(){
     printf("Thank you for utilizing oss. Exiting...n");
 }
+
+
 void help_message(){
     printf("Welcome to timed workers program\n");
     printf("DESCRIPTION:");
@@ -176,6 +262,8 @@ void help_message(){
     printf("-t arg, where arg is the alloted time a worker will hang around\n");
     printf("\n\nEXAMPLE:\n./oss -n 3 -s 2 -t 4\nThe following example above will produce 3 workers\nof which 2 of the workers will work in synchronous\nand will assign a time between 1 and 4 seconds for a worker to hang around\n");
 }
+
+
 int digits_only(const char* string){
     int exit = 1;
     int i;
@@ -190,16 +278,29 @@ int digits_only(const char* string){
     return exit;
 }
 ///END/////////////////////////////////////////
+
+
+
+
+
 ///ioss.h//////////////////////////////////////
+
 void child(){
     char* prgnm = "./worker";
     char sec_str[12];
     char nano_str[12];
-    sprintf(sec_str, "%d", seconds);
-    sprintf(nano_str, "%d", nanoseconds);
+    int randsec = getrandsec();
+    int randnano = getrandnano();
+    sprintf(sec_str, "%d", randsec);
+    sprintf(nano_str, "%d", randnano);
     char* args[] = {prgnm, sec_str, nano_str, NULL};
     execv(prgnm, args);
 }
+
+
+
+
+
 void parent(pid_t child, int process_num){
     struct PCB childBlock;
     childBlock.pid = child;
@@ -207,10 +308,20 @@ void parent(pid_t child, int process_num){
     childBlock.start_nano = nanoseconds;
     childBlock.occupied = process_num;
 }
+
+
+
+
+
 void check_if_child_has_terminated(){
     int status;
     int pid = waitpid(-1, &status, WNOHANG);
 }
+
+
+
+
+
 void print_process_table(){
     printf("OSS PID: %d SysClockS: %d SysclockNano: %d\n", getpid(), seconds, nanoseconds);
     printf("Process Table:\n");
@@ -220,6 +331,11 @@ void print_process_table(){
         printf("%d\t%d\t\t%d\t%d\t%d\n",i,process_table[i].occupied,process_table[i].pid,process_table[i].start_seconds,process_table[i].start_nano);
     }
 }
+
+
+
+
+
 void increment_clock(){
     if(nanoseconds > 999999999){
         seconds++;
--- worker.c
+++ worker.c
@@ -3,21 +3,35 @@
 #include <unistd.h>
 #include <sys/shm.h>
 #include "shrdmem.h"
+
 int main(int argc, char* argv[]){
+
+
+
+
+
+    printf("seconds given: %s                ", argv[1]);
+    printf("nano given: %s\n", argv[2]);
+
+
+
     int secondsStay, nanoStay;
     int shmid = shmget(SHMKEY_1, BUff_SZ, 0777);
+    
     if(shmid == -1){
-        fprintf(stderr, "Child:...Error in shmget...");
+        fprintf(stderr, "Child:...Error in shmget...\n");
         return 1;
     }
 
     int* cint = (int*)(shmat(shmid, 0, 0));
     printf("seconds from shrdmem %d\n", cint[0]);
     printf("nanoseconds from shrdmem %d\n", cint[1]);
+    
     // secondsStay = atoi(argv[1]);
     // nanoStay = atoi(argv[2]);
     printf("Child about to sleep\n");
-    sleep(2);
+    sleep(7);
     printf("Child done\n");
+    shmdt(cint);
     return 0;
 }
