1971580dad6121b5b5ec816ae6ac784a4880c39c
Marco Preciado <mac46005@gmail.com>
2023-02-27 08:01:19 -0600

    update rpo

--- oss.c
+++ oss.c
@@ -1,10 +1,6 @@
-//FILE: oss.c
-//AUTHOR: Marco Preciado
-//DESCRIPTION: 
-
-
-
-
+// FILE: oss.c
+// AUTHOR: Marco Preciado
+// DESCRIPTION:
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,24 +14,16 @@
 #include "ioss.h"
 #include "shrdmem.h"
 
-
-
-
-
-
-
-
-
-
-
-struct PCB{
+struct PCB
+{
     int occupied;
     pid_t pid;
     int start_seconds;
     int start_nano;
 };
 
-struct PCB create_pcb(int occupied,pid_t pid, int start_seconds, int start_nano){
+struct PCB create_pcb(int occupied, pid_t pid, int start_seconds, int start_nano)
+{
     struct PCB new_child;
     new_child.occupied = occupied;
     new_child.pid = pid;
@@ -46,145 +34,157 @@ struct PCB create_pcb(int occupied,pid_t pid, int start_seconds, int start_nano)
 
 struct PCB process_table[20];
 
-
-
-
-
-
-
-
-
-
-int getrandsec(){
+int getrandsec()
+{
     srand(getpid());
     int num = (rand() % time_limit + 1);
     return num;
 }
-int getrandnano(){
+int getrandnano()
+{
     srand(getpid());
     int num = (rand() % 999999999);
     return num;
 }
 
+int main(int argc, char *argv[])
+{
 
-
-
-
-
-
-
-
-
-int main(int argc, char* argv[]){
-    
-    
-    
-    int status,count = 0, a_child_terminated = 0;
+    int status, count = 0, a_child_terminated = 0;
 
     pid_t pid;
     pid_t terminated_child;
     pid_t simul_pid[simul];
 
-
     int shm_id = shmget((key_t)SHMKEY, BUff_SZ, 0666 | IPC_CREAT);
+    int *paddr = (int *)shmat(shm_id, NULL, 0);
+    if (process_getopt(argc, argv) == 0)
+    {
 
-    if(process_getopt(argc, argv) == 0){
-
-        while(simul != 0){
+        while (proc != 0)
+        {
             increment_clock();
-            int* paddr = (int*)shmat(shm_id, NULL, 0);
+            
             paddr[0] = seconds;
             paddr[1] = nanoseconds;
-            pid = fork();
-            simul--;
-            if(pid == 0){
-                child();
-            }else{
-                struct PCB new_child = create_pcb(1, pid, seconds,nanoseconds);
-                process_table[count] = new_child;
-                //print_process_table();
-                if((terminated_child = waitpid(pid, &status, WNOHANG)) == 0){
-                    a_child_terminated = 1;
-                    process_table[count].occupied = 0;
+            if (simul != 0)
+            {
+
+                pid = fork();
+                if (pid == 0)
+                {
+                    child();
                 }
-                //print_process_table();
-                count++;
+                else
+                {
+                    struct PCB new_child = create_pcb(1, pid, seconds, nanoseconds);
+                    process_table[count] = new_child;
+                    // print_process_table();
+                    terminated_child = waitpid(pid, &status, WNOHANG);
+                    if (WEXITSTATUS(status))
+                    {
+                        a_child_terminated = 1;
+                        process_table[count].occupied = 0;
+                    }
+                    if(seconds == 500000000){
+                        print_process_table();
+                    }
+                    count++;
+                    proc--;
+                    simul--;
+                }
+            }else{
+                pid = fork();
+                if(pid == 0){
+                    child();
+                }else{
+                    struct PCB new_child = create_pcb(1, pid, seconds, nanoseconds);
+                    process_table[count] = new_child;
+                    wait(NULL);
+                    process_table[count].occupied = 0;
+                    count--;
+                }
             }
         }
-        
-    }else{
+    }
+    else
+    {
         help_message();
         exit(1);
     }
-    
+
     exit(0);
 }
 
-
-
-
-
-
-
-
-
-//igetopt_helper.h/////////////////////////////
-int process_getopt(int argc, char* argv[]){
-    const char* options = "hn:s:t:";
+// igetopt_helper.h/////////////////////////////
+int process_getopt(int argc, char *argv[])
+{
+    const char *options = "hn:s:t:";
     char val[200];
     char opt;
     int exit = 1;
-    while((opt = getopt(argc, argv, options)) != -1){
-        switch(opt){
-            case 'h':
-                help_message();
-                exit = 1;
-                break;
-            case 'n':
-                if(digits_only(optarg) == 0){
-                    proc = atoi(optarg);
-                    exit = 0;
-                }else{
-                    option_err(opt, optarg);
-                }
-                break;
-            case 's':
-                if(digits_only(optarg) == 0){
-                    simul = atoi(optarg);
-                    exit = 0;
-                }else{
-                    option_err(opt, optarg);
-                }
-                break;
-            case 't':
-                if(digits_only(optarg) == 0){
-                    time_limit = atoi(optarg);
-                    exit = 0;
-                }else{
-                    option_err(opt, optarg);
-                }
-                break;
-            default:
-                help_message();
-                exit = 1;
-                break;
+    while ((opt = getopt(argc, argv, options)) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            help_message();
+            exit = 1;
+            break;
+        case 'n':
+            if (digits_only(optarg) == 0)
+            {
+                proc = atoi(optarg);
+                exit = 0;
+            }
+            else
+            {
+                option_err(opt, optarg);
+            }
+            break;
+        case 's':
+            if (digits_only(optarg) == 0)
+            {
+                simul = atoi(optarg);
+                exit = 0;
+            }
+            else
+            {
+                option_err(opt, optarg);
+            }
+            break;
+        case 't':
+            if (digits_only(optarg) == 0)
+            {
+                time_limit = atoi(optarg);
+                exit = 0;
+            }
+            else
+            {
+                option_err(opt, optarg);
+            }
+            break;
+        default:
+            help_message();
+            exit = 1;
+            break;
         }
     }
     return exit;
 }
 
-
-void option_err(char opt, char* value){
+void option_err(char opt, char *value)
+{
     fprintf(stderr, "ERROR: option %c has invalid value %s\n", opt, value);
 }
 
-
-void exit_message(){
+void exit_message()
+{
     printf("Thank you for utilizing oss. Exiting...n");
 }
 
-
-void help_message(){
+void help_message()
+{
     printf("Welcome to timed workers program\n");
     printf("DESCRIPTION:");
     printf("Please input a positive integer for the following options\n");
@@ -194,45 +194,43 @@ void help_message(){
     printf("\n\nEXAMPLE:\n./oss -n 3 -s 2 -t 4\nThe following example above will produce 3 workers\nof which 2 of the workers will work in synchronous\nand will assign a time between 1 and 4 seconds for a worker to hang around\n");
 }
 
-
-int digits_only(const char* string){
+int digits_only(const char *string)
+{
     int exit = 1;
     int i;
     int length = strlen(string);
-    for(i = 0; i < length; i++){
-        if(string[i] >= '0' && string[i] <= '9'){
+    for (i = 0; i < length; i++)
+    {
+        if (string[i] >= '0' && string[i] <= '9')
+        {
             exit = 0;
-        }else{
+        }
+        else
+        {
             exit = 1;
         }
     }
     return exit;
 }
-///END/////////////////////////////////////////
+/// END/////////////////////////////////////////
 
+/// ioss.h//////////////////////////////////////
 
-
-
-
-///ioss.h//////////////////////////////////////
-
-void child(){
-    char* prgnm = "./worker";
+void child()
+{
+    char *prgnm = "./worker";
     char sec_str[12];
     char nano_str[12];
     int randsec = getrandsec();
     int randnano = getrandnano();
     sprintf(sec_str, "%d", randsec);
     sprintf(nano_str, "%d", randnano);
-    char* args[] = {prgnm, sec_str, nano_str, NULL};
+    char *args[] = {prgnm, sec_str, nano_str, NULL};
     execv(prgnm, args);
 }
 
-
-
-
-
-void parent(pid_t child, int process_num){
+void parent(pid_t child, int process_num)
+{
     struct PCB childBlock;
     childBlock.pid = child;
     childBlock.start_seconds = seconds;
@@ -240,39 +238,34 @@ void parent(pid_t child, int process_num){
     childBlock.occupied = process_num;
 }
 
-
-
-
-
-void check_if_child_has_terminated(){
+void check_if_child_has_terminated()
+{
     int status;
     int pid = waitpid(-1, &status, WNOHANG);
 }
 
-
-
-
-
-void print_process_table(){
+void print_process_table()
+{
     printf("OSS PID: %d SysClockS: %d SysclockNano: %d\n", getpid(), seconds, nanoseconds);
     printf("Process Table:\n");
     printf("Entry\tOccupied\tPID\tStarts\tStartN\n");
     int i;
-    for(i = 0; i < 20; i++){
-        printf("%d\t%d\t\t%d\t%d\t%d\n",i,process_table[i].occupied,process_table[i].pid,process_table[i].start_seconds,process_table[i].start_nano);
+    for (i = 0; i < 20; i++)
+    {
+        printf("%d\t%d\t\t%d\t%d\t%d\n", i, process_table[i].occupied, process_table[i].pid, process_table[i].start_seconds, process_table[i].start_nano);
     }
 }
 
-
-
-
-
-void increment_clock(){
-    if(nanoseconds > 999999999){
+void increment_clock()
+{
+    if (nanoseconds > 999999999)
+    {
         seconds++;
         nanoseconds = 0;
-    }else{
+    }
+    else
+    {
         nanoseconds += 10000;
     }
 }
-///END/////////////////////////////////////////
+/// END/////////////////////////////////////////
--- worker.c
+++ worker.c
@@ -8,6 +8,18 @@ void print_worker(int sys_sec, int sys_nano){
     printf("WORKER PID:%d PPID:%d SysclockS:%d SysclockNano:%d\n", getpid(), getppid(), sys_sec, sys_nano);
 }
 
+int get_sec_stay_time(int given_sec,int start_sec){
+    return start_sec + given_sec;
+}
+
+int get_nano_stay_time(int given_nano,int start_nano){
+    if(start_nano > 999999999){
+        return given_nano;
+    }
+    return start_nano + given_nano;
+}
+
+
 int main(int argc, char* argv[]){
 
 
@@ -17,24 +29,36 @@ int main(int argc, char* argv[]){
     
 
 
-    int secondsStay, nanoStay;
+    int given_sec, given_nano;
     int shmid = shmget(SHMKEY, BUff_SZ, 0666);
 
     if(shmid == -1){
         fprintf(stderr, "Child:...Error in shmget...\n");
         return 1;
     }
-    int* cint = (int*)(shmat(shmid, NULL, 0));
+    int* shared_mem_address = (int*)(shmat(shmid, NULL, 0));
 
 
-    secondsStay = atoi(argv[1]);
-    nanoStay = atoi(argv[2]);
-    printf("Given rand sec: %d nano: %d\n", secondsStay, nanoStay);
-    print_worker(cint[0], cint[1]);
+    given_sec = atoi(argv[1]);
+    given_nano = atoi(argv[2]);
+    
 
-    sleep(10);
+    print_worker(shared_mem_address[0], shared_mem_address[1]);
+    int sec_stay = get_sec_stay_time(given_sec, shared_mem_address[0]);
+    int nano_stay = get_nano_stay_time(given_nano, shared_mem_address[1]);
 
+    printf("pid: %d sec_stay:%d\n", getpid(),sec_stay);
+    printf("pid: %d nano_stay:%d\n",getpid(),nano_stay);
 
-    shmdt(cint);
+    int count = 0;
+    printf("about to loop worker till time\n");
+    while(sec_stay > shared_mem_address[0] && nano_stay > shared_mem_address[1]){
+            printf("%d %d:%d\n",getpid(), shared_mem_address[0], shared_mem_address[1]);
+    }
+
+    print_worker(shared_mem_address[0], shared_mem_address[1]);
+    printf("--Terminating\n");
+
+    shmdt(shared_mem_address);
     return 0;
 }
